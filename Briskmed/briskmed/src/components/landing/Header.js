974b17866759a948cb8e801ab28da0a7ed816fed
ij03l <idahorjoel@gmail.com>
2022-12-04 08:48:00 +0100

    updating Styling and finalising build

--- Briskmed/briskmed/src/components/landing/Header.js
+++ Briskmed/briskmed/src/components/landing/Header.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import circle from '../../assets/svg/Vector.svg';
 import search from '../../assets/svg/3d-search.svg';
 
 const Header = () => {
-  return (
-    <div className="w-full flex flex-col">
-        <div className='w-full mt-[-5rem] relative justify-center items-center flex flex-row'>
-            <div className='search mr-[-40rem]'>
-                <img src={search} alt="3d-search-img" /> 
-            </div>
-            <div className='search-bar'>
-                <div className='grp'>
-                <img src={circle} alt="circle-placeholder" />
-                <input type="text" placeholder='Hospital search..' />
+    const [text, setText] = useState("")
+
+    return (
+        <div className="w-full flex flex-col">
+            <div className='w-full mt-[-5rem] relative justify-center items-center flex flex-row'>
+                <div className='search mr-[-40rem]'>
+                    <img src={search} alt="3d-search-img" />
+                </div>
+                <div className='search-bar'>
+                    <div className='grp'>
+                        <a href={`/search/${text}`}>
+                            <img src={circle} alt="circle-placeholder" />
+                        </a>
+
+                        <input onChange={(e) => setText(e.target.value)} type="text" placeholder='Hospital search..' />
+                    </div>
                 </div>
             </div>
-        </div>
         <div className='page-description mt-[-7rem] relative uppercase flex justify-center items-center'>
             <h1>Making Healthcare access, fast and effective</h1>
         </div>
--- Briskmed/briskmed/src/components/landing/Headerx.js
+++ Briskmed/briskmed/src/components/landing/Headerx.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import circle from '../../assets/svg/Vector.svg';
 
 
 const Headerx = () => {
+  const [text, setText] = useState("")
+
   return (
     <div className="w-full flex flex-col">
-        <div className='w-full mt-[-5rem] relative justify-center items-center flex flex-row'>
-            <div className='search-bar'>
-                <div className='grp'>
-                <img src={circle} alt="circle-placeholder" />
-                <input type="text" placeholder='Hospital search..' />
-                </div>
-            </div>
+      <div className='w-full mt-[-5rem] relative justify-center items-center flex flex-row'>
+        <div className='search-bar'>
+          <div className='grp'>
+            <a href={`/search/${text}`}>
+              <img src={circle} alt="circle-placeholder" />
+            </a>
+            <input onChange={(e) => setText(e.target.value)} type="text" placeholder='Hospital search..' />
+          </div>
         </div>
-        
+      </div>
+
     </div>
   )
 }
